640527c6b77b7862bf46a94c79bf5fafde4ff10f
Zeke <ezequieldapena@gmail.com>
2020-02-26 17:32:53 -0300

    me canse

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function updateSize() {
   vh = window.innerHeight * 0.01;
   vw = window.innerWidth * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-
+  
 }
 window.addEventListener("resize", updateSize)
 updateSize();
@@ -17,58 +17,64 @@ function random(min, max) {
 const app = new Vue({
   el:"#app",
   data:{
-    qod:"",
-    modulos:[]
+    pages:[]
   },
   methods:{},
   created(){
-
+    
     async function fetchAll(){
-      let resQuotes = await fetch("./json/quotes.json");
-      let jsonQuotes = await resQuotes.json();
-
-      let resModulos = await fetch("./json/modulos.json");
-      let jsonModulos = await resModulos.json();
+      let resPages = await fetch("./json/pages.json");
+      let json = await resPages.json();
       
-      return [jsonQuotes,jsonModulos]
+      return json
     }
-
     fetchAll()
     .then(data => {
-      const index =random(0, data[0].quotes.length-1);
-      app.qod=data[0].quotes[index];
-      app.modulos=data[1];
-
+      app.pages=data.pages;
+      
     })
   },
   computed:{},
   components:{
-    cards:{
+    principal:{
       props:['array'],
       template:`
-      <div id="playfield" class="col-12 d-flex justify-content-around flex-wrap py-2">
-        <div v-for="modulo in array" class="flip-card my-2">
-          <div class="flip-card-inner">
-            <div class="flip-card-front" :style="modulo.imagen">
-              <h4>{{modulo.nombre}}</h4>
-            </div>
-            <div class="flip-card-back p-2">
-              <h1>{{modulo.titulo}}</h1>
-              <p>{{modulo.detalle}}</p>
-              <a :href='modulo.url' class="btn btn-purple mb-0" target="_blank" rel="noopener noreferrer">Ir al micrositio</a>
-            </div>
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+   
+        <div class="carousel-inner">
+        <div class="carousel-item active slide" id="linkHTML" :style="array[0].bg">
+          <div class="d-flex justify-content-center flex-column align-items-center" >
+            <h2>{{array[0].nombre}}</h2>
+            <a href="HTMLpage/html.html" class="btn btn-primary btn-lg" role="button" aria-disabled="true">Go to HTML!</a>
           </div>
         </div>
-      </div>       
-      `
-    },
-    quote:{
-      props:["quote"],
-      template:`
-      <blockquote class="blockquote text-right mx-5">
-        <p class="mb-0">{{quote.text}}</p>
-        <div class="blockquote-footer">{{quote.author ? quote.author : "Anonimous"}}</div>
-      </blockquote>
+        <div class="carousel-item slide" id="linkCSS" :style="array[1].bg">
+          <div class="d-flex justify-content-center flex-column align-items-center" >
+            <h2>{{array[1].nombre}}</h2>
+            <a href="CSSpage/css.html" class="btn btn-primary btn-lg" role="button" aria-disabled="true">Go to CSS!</a>
+          </div>
+        </div>
+        <div class="carousel-item slide" id="linkJS" :style="array[2].bg">
+          <div class="d-flex justify-content-center flex-column align-items-center" >
+            <h2>{{array[2].nombre}}</h2>
+            <a href="JSpage/js.html" class="btn btn-primary btn-lg" role="button" aria-disabled="true">Go to JS!</a>
+          </div>
+        </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+      </a>
+      </div>
+      </div>
       `
     }
   }
